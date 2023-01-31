5689e0dcc4f33d7bb0af193ef76ffb1060d33f66
Mcc <macharitocruzuk@gmail.com>
2023-01-31 14:27:16 +0000

    working version - search button function, weatherhead function, 5 day weather forecast.

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,7 +121,7 @@ function weatherHead(weatherData) {
   // console.log(currentCityWeather);
 }
 function nextWeather(weatherData) {
-  let nextDay = moment(1675231200, "X").format("DD / MM / YYYY");
+  let nextDay = moment(1675231200, "X").format("DD / MM / YYYY hh:mm");
   let dataWeather = weatherData.list[5].weather[0].icon;
   let icon1 = `https://openweathermap.org/img/wn/${dataWeather}@2x.png`;
   let nextTemp = Math.floor(weatherData.list[5].main.temp - 273.15);
@@ -143,12 +143,12 @@ function nextWeather(weatherData) {
 
   day1.innerHTML = nextDayWeather;
 
-  let day02 = moment(1675231200, "X").format("DD / MM / YYYY");
-  let dataWeather2 = weatherData.list[5].weather[0].icon;
+  let day02 = moment(1675317600, "X").format("DD / MM / YYYY hh:mm");
+  let dataWeather2 = weatherData.list[13].weather[0].icon;
   let icon2 = `https://openweathermap.org/img/wn/${dataWeather2}@2x.png`;
-  let nextTemp2 = Math.floor(weatherData.list[5].main.temp - 273.15);
+  let nextTemp2 = Math.floor(weatherData.list[13].main.temp - 273.15);
 
-  console.log("date" + day02 + " icon " + icon1 + " temp " + nextTemp);
+  console.log("date" + day02 + " icon " + icon2 + " temp " + nextTemp2);
 
   let Day2Weather = `<div  class="card" style="width: 12rem">
   <div class="card-body">
@@ -158,12 +158,78 @@ function nextWeather(weatherData) {
   </div>
   <ul class="list-group list-group-flush">
     <li class="list-group-item">Temp: ${nextTemp2} &#8451</li>
-    <li class="list-group-item">Wind: ${weatherData.list[5].wind.speed} KPH</li>
-    <li class="list-group-item">Humidity:${weatherData.list[5].main.humidity}%</li>
+    <li class="list-group-item">Wind: ${weatherData.list[13].wind.speed} KPH</li>
+    <li class="list-group-item">Humidity:${weatherData.list[13].main.humidity}%</li>
   </ul>
 </div>`;
 
   day2.innerHTML = Day2Weather;
+
+  let day03 = moment(1675404000, "X").format("DD / MM / YYYY hh:mm");
+  let dataWeather3 = weatherData.list[21].weather[0].icon;
+  let icon3 = `https://openweathermap.org/img/wn/${dataWeather3}@2x.png`;
+  let nextTemp3 = Math.floor(weatherData.list[21].main.temp - 273.15);
+
+  console.log("date" + day03 + " icon " + icon3 + " temp " + nextTemp3);
+
+  let Day3Weather = `<div  class="card" style="width: 12rem">
+  <div class="card-body">
+    <h5 class="card-title">${day03}</h5>
+
+    <img class="card-img-top" src="${icon3}" alt="Card image cap" />
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Temp: ${nextTemp3} &#8451</li>
+    <li class="list-group-item">Wind: ${weatherData.list[21].wind.speed} KPH</li>
+    <li class="list-group-item">Humidity:${weatherData.list[21].main.humidity}%</li>
+  </ul>
+</div>`;
+
+  day3.innerHTML = Day3Weather;
+
+  let day04 = moment(1675490400, "X").format("DD / MM / YYYY hh:mm");
+  let dataWeather4 = weatherData.list[29].weather[0].icon;
+  let icon4 = `https://openweathermap.org/img/wn/${dataWeather4}@2x.png`;
+  let nextTemp4 = Math.floor(weatherData.list[29].main.temp - 273.15);
+
+  console.log("date" + day04 + " icon " + icon4 + " temp " + nextTemp4);
+
+  let Day4Weather = `<div  class="card" style="width: 12rem">
+  <div class="card-body">
+    <h5 class="card-title">${day04}</h5>
+
+    <img class="card-img-top" src="${icon4}" alt="Card image cap" />
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Temp: ${nextTemp3} &#8451</li>
+    <li class="list-group-item">Wind: ${weatherData.list[29].wind.speed} KPH</li>
+    <li class="list-group-item">Humidity:${weatherData.list[29].main.humidity}%</li>
+  </ul>
+</div>`;
+
+  day4.innerHTML = Day4Weather;
+
+  let day05 = moment(1675576800, "X").format("DD / MM / YYYY hh:mm");
+  let dataWeather5 = weatherData.list[37].weather[0].icon;
+  let icon5 = `https://openweathermap.org/img/wn/${dataWeather5}@2x.png`;
+  let nextTemp5 = Math.floor(weatherData.list[37].main.temp - 273.15);
+
+  console.log("date" + day05 + " icon " + icon5 + " temp " + nextTemp5);
+
+  let Day5Weather = `<div  class="card" style="width: 12rem">
+  <div class="card-body">
+    <h5 class="card-title">${day05}</h5>
+
+    <img class="card-img-top" src="${icon4}" alt="Card image cap" />
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Temp: ${nextTemp5} &#8451</li>
+    <li class="list-group-item">Wind: ${weatherData.list[37].wind.speed} KPH</li>
+    <li class="list-group-item">Humidity:${weatherData.list[37].main.humidity}%</li>
+  </ul>
+</div>`;
+
+  day5.innerHTML = Day5Weather;
 }
 
 // console.log(weatherData.list);
